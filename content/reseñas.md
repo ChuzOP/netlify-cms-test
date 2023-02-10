95a58e882dda66cec8cfdb507ef73fd186a79aa1
chuso <dejesusgarciaop@gmail.com>
2023-02-09 23:01:33 -0600

    add section reseñas

---
+++ content/reseñas.md
@@ -0,0 +1,10 @@
+---
+titulo: Reseñas
+reseñas:
+  - descripcion: "Fino"
+    autor: JuanDueño
+  - descripcion: "Test"
+    autor: Chus
+  - descripcion: "Testing from netlify"
+    autor: dev
+---
---
+++ pages/blog.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Head from "next/head";
+import { attributes, react as ReseñasContent } from "../content/reseñas.md";
+
+export const Blog = () => {
+  const { titulo, reseñas } = attributes;
+  return (
+    <>
+      <Head>
+        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+      </Head>
+      <main>
+        <h1>{titulo}</h1>
+        <div>
+          {reseñas.map((reseña) => (
+            <div>
+              <h4>{reseña.autor}</h4>
+              <p> {reseña.descripcion} </p>
+            </div>
+          ))}
+        </div>
+      </main>
+    </>
+  );
+};
